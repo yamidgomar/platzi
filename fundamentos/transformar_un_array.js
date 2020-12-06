8eabf978da3d90d1b23695eb8a82d96caae63b37
Yamid Gonzalez <yamidgm@gmail.com>
2020-12-06 18:21:10 -0500

    actualizado transformar_un_array.js

--- fundamentos/transformar_un_array.js
+++ fundamentos/transformar_un_array.js
@@ -1,3 +1,6 @@
+//El método map() itera sobre los elementos de un array en el orden de inserción y devuelve array nuevo con los elementos modificados.
+//Ejercicio pasar la altura de la personas de metros a centimetros.
+
 var sacha = {
         nombre: 'Sacha',
         apellido: 'Lifszyc',
@@ -41,4 +44,30 @@ var paula = {
 }
 
 var personas = [sacha, alan, martin, dario, vicky, paula]
-console.log(personas[0])
+
+/*const pasarAlturaACentimetros = persona =>{
+    //persona.altura * 100
+    //persona.altura *= 100
+    //de la manera anterior modificamos el array personas, para no cambiarlo
+    //retornamos un nuevo objeto solo cvon la altura modificada asi:
+    return {
+        ...persona,
+        altura: persona.altura * 100
+    } 
+}*/
+
+
+//Para hacer la funcion un poco mas prolijo:
+//Como vamos a devolver un objeto debemos poner parentesis envolviendolo:
+const pasarAlturaACentimetros = persona =>({
+    ...persona,
+    altura: persona.altura * 100
+})
+
+
+var personasCms = personas.map(pasarAlturaACentimetros)
+console.log(personasCms)
+console.log(personas)
+
+//son objetos totalmente distintos.
+console.log(personas[0] === personasCms[0])
