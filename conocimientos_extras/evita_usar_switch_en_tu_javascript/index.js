4c7172c47a5eb50760b52a6f428c223c10baa226
yamid gonzalez <yamidgm@gmail.com>
2021-05-26 22:24:03 -0500

    actualizando android

--- conocimientos_extras/evita_usar_switch_en_tu_javascript/index.js
+++ conocimientos_extras/evita_usar_switch_en_tu_javascript/index.js
@@ -1,7 +1,7 @@
 //https://youtu.be/0NlsJuwFsrQ
 //Lista de reproduccion html, css, javascript, nodejs.
 
-const adversario = 'Thor';
+const adversario = 'Espiderman';
 
 
 // ====== Con switch ====== \\
@@ -11,59 +11,67 @@ const adversario = 'Thor';
 
 let loki = ''
 switch (adversario){
-    case 'Iron-Man':
-        loki = 'Magneto'
-        break
+case 'Iron-Man':
+    loki = 'Magneto'
+    break
 
-    case 'Hulk':
-        loki = 'Thanos'
-        break
-    case 'Thor':
-        loki = 'Odin'
-        break
-    default:
-        loki = 'Loki'
+case 'Hulk':
+    loki = 'Thanos'
+    break
+case 'Thor':
+    loki = 'Odin'
+    break
+default:
+    loki = 'Loki'
 }
 
 // ====== Con If ====== \\ 
 let loki = ''
 if(adversario == 'Iron-Man'){
-    loki = 'Magneto'
+loki = 'Magneto'
 }else if(adversario == 'Hulk'){
-    loki = 'Thanos'
+loki = 'Thanos'
 }else if(adversario == 'Thor'){
-    loki = 'Odin'
+loki = 'Odin'
 }else{
-    loki = 'Loki'
+loki = 'Loki'
 }
 
 // ====== Con un Objeto ====== \\
 
 const LOKI_DISFRASES = {
-    'Iron-man': 'Magneto',
-    Hulk:'Thanos',
-    Tor: 'Odin'
+'Iron-man': 'Magneto',
+Hulk:'Thanos',
+Tor: 'Odin'
 }
 
 const LOKI_DEFAULT = 'Loki'
 const loki = LOKI_DISFRASES[adversario] || LOKI_DEFAULT
 
 const LOKI_DISFRASES = {
-    'Iron-man': () => 'Magneto',
-    Hulk: () => 'Thanos',
-    Tor: () => 'Odin'
+'Iron-man': () => 'Magneto',
+Hulk: () => 'Thanos',
+Tor: () => 'Odin'
 }
 
 const LOKI_DEFAULT = 'Loki'
 const loki = LOKI_DISFRASES[adversario]?LOKI_DISFRASES[adversario]():LOKI_DEFAULT
 */
-const LOKI_DISFRASES = {
-    'Iron-Man': ()=>'Magneto',
-    Hulk:()=>'Thanos',
-    Thor:()=>'Odin'
-}
 
-const LOKI_DEFAULT = 'Loki'
-const loki = LOKI_DISFRASES[adversario]?LOKI_DISFRASES[adversario]():LOKI_DEFAULT;
+var loki = ''
+
+switch(adversario){
+    case 'Thor':
+        loki = 'Odin'
+        break
+    case 'Iron-Man':
+        loki = 'Magneto'
+        break
+    case 'Hulk':
+        loki = 'Thanos'
+        break
+    default:
+        loki = 'Loki'
+}
 
 console.log(loki)
