44ca036b11213ed7d09d831c478f4c4634c3dd50
yamid gonzalez <yamidgm@gmail.com>
2021-06-23 20:27:50 -0500

    prueba...

--- Curso_de_estructuras_de_datos_en_javascript/prueba/index.js
+++ Curso_de_estructuras_de_datos_en_javascript/prueba/index.js
@@ -16,37 +16,15 @@ class myArray{
 
     pop(){
         const lastItem = this.data[this.length -1]
-        delete this.data[this.length -1]
-        this.length--
-        console.log(lastItem)
-    }
-
-    delete(index){
-        const item = this.data[index]
-        this.shifIndex(index)
-        console.log(item)
-    }
-
-    shifIndex(index){
-        for(let i = index;i < this.length - 1; i++){
-            this.data[i] = this.data[i + 1]
-        }
-
-        delete this.data[this.length -1]
+        delete this.data[this.length - 1]
         this.length--
     }
-
-    
 }
 
 const myarray = new myArray()
 
-//myarray.get(0)
 myarray.push('maria')
-myarray.push('alejandro')
 myarray.push('yago')
-//myarray.pop()
-
-console.log(myarray)
-myarray.delete(1)
+myarray.push('yamid')
+myarray.pop()
 console.log(myarray)
