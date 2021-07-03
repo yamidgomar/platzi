76d2b8d745ca6b2131c142c8cbda3be654f1fd57
yamid gonzalez <yamidgm@gmail.com>
2021-07-03 02:05:46 -0500

    actualizando android

--- Curso_de_estructuras_de_datos_en_javascript/prueba/index.js
+++ Curso_de_estructuras_de_datos_en_javascript/prueba/index.js
@@ -1,4 +1,4 @@
-class myArray{
+/*vclass myArray{
     constructor(){
         this.length = 0
         this.data = {}
@@ -28,3 +28,45 @@ myarray.push('yago')
 myarray.push('yamid')
 myarray.pop()
 console.log(myarray)
+*/
+
+class MyArray{
+    constructor(){
+        this.length = 0;
+        this.data = {}
+    }
+
+    get(index){
+        console.log(this.data[index])
+    }
+
+    push(item){
+        this.data[this.length] = item
+        console.log(this.data)
+        this.length++
+    }
+
+    pop(){
+        const lastItem = this.data[this.length -1 ]
+        delete this.data[this.length -1 ]
+        this.length--
+        return lastItem
+    }
+
+    unshif(){
+        const firtItem = this.data[0]
+        delete this.data[0]
+        return firtItem
+    }
+}
+
+var myarray = new MyArray()
+
+myarray.push('yago')
+myarray.push('yamid')
+myarray.push('maria')
+myarray.get(0)
+console.log(myarray.pop())
+console.log(myarray.unshif())
+console.log(myarray)
+
